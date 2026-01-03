a9547c5e1b70e41434e41a6fa17298d85739f1eb
Basit Ali <basitaliburiro1110@gmail.com>
2026-01-03 19:13:49 +0500

    Initial project setup with folder structure

--- practice/app/page.js
+++ practice/app/page.js
@@ -1,7 +1,10 @@
-
 export default function Home() {
- 
   return (
-   <></>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-bold">Welcome to Next.js</h1>
+        <p className="text-lg mt-4">Get started by editing this page.</p>
+      </div>
+    </main>
   );
 }
